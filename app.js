0cce81dfc5f70e969f680faf41d068252b181089
user444 <104263986+nouname0@users.noreply.github.com>
2023-01-28 17:36:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function first() {
+    // Про 10+50 кнопка
+    alert("Будет ${50+10}")
+}
+function second() {
+
+    let age = prompt("Введи свой возраст:")
+    if (age < 18) {
+    alert("Ты маленький(ая)")
+    } else {
+        alert("Вход")
+    }
+}
+function third() {
+    // Вставить а элемент HTML c id "zp" вставить тысячу
+    document.getElementById("zp").innerHTML = "1000$"
+}
+
